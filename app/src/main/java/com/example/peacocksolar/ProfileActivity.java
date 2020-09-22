631fdecead3351144bfbb351973c0f980265d2cb
mr-pramod-98 <mr.pj2k18@gmail.com>
2020-09-22 18:49:27 +0530

    Added DatePicker and other minor changes

--- app/src/main/java/com/example/peacocksolar/ProfileActivity.java
+++ app/src/main/java/com/example/peacocksolar/ProfileActivity.java
@@ -1,21 +1,31 @@
 package com.example.peacocksolar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class ProfileActivity extends AppCompatActivity {
 
     // WIDGETS
     private Toolbar toolbar;
     private EditText nameEditText, emailEditText, phoneEditText;
+    private TextView dobEditText;
     private Spinner genderSpinner;
     private Button saveButton;
 
@@ -29,10 +39,34 @@ public class ProfileActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.email_edit_text_profile);
         phoneEditText = findViewById(R.id.phone_edit_text_profile);
         genderSpinner = findViewById(R.id.gender_spinner_profile);
+        dobEditText = findViewById(R.id.date_of_birth_edit_text_profile);
         saveButton = findViewById(R.id.save_button);
 
         // DISABLE EMAIL FIELD SINCE "EMAIL SHOULD BE UNIQUE" AND CANNOT BE CHANGED
         emailEditText.setEnabled(false);
+
+
+        dobEditText.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                // Use the current date as the default date in the picker
+                final Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ProfileActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        String date = day + "/" + month + "/" + year;
+                        dobEditText.setText(date);
+                    }
+                }, year, month, day);
+
+                datePickerDialog.show();
+            }
+        });
 
 
         /* ====================== SPINNER GENDER SETUP -> START ========================*/
